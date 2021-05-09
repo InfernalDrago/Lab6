90d146d5a04cf83f9bf070249e073464571e0d0b
Ricardo Ledezma Guerrero <72947284+InfernalDrago@users.noreply.github.com>
2021-05-08 20:08:39 -0700

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -19,11 +19,11 @@ document.addEventListener('DOMContentLoaded', () => {
   let url = "https://cse110lab6.herokuapp.com/entries"  // SET URL 
 
   fetch(url)
-    .then(response => response.json)
+    .then(response => response.json())
     .then(entries => {
       entries.forEach((entry) => {
        
-        let newPost;  
+        let newPost;
 
 
         // CODE GOES HERE vvv
